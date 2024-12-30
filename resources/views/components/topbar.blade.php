d38ee9ab3e89ccc2007105d8daafde8a13371c88
arian.kapllanaj <arian.kapllanaj@icloud.com>
2024-12-30 08:17:37 +0100

    final changes in topbar

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -1,11 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <style>
+        /* Navigation button hover effect */
+        .nav-link {
+            position: relative;
+            transition: color 0.3s ease, transform 0.3s ease;
+        }
+
+        .nav-link:hover {
+            color: #88C5AD;
+            /* Change to your desired hover color */
+            transform: translateY(-3px);
+            /* Slight lift effect */
+        }
+
+        .nav-link::after {
+            content: "";
+            display: block;
+            width: 0;
+            height: 2px;
+            background: #88C5AD;
+            transition: width 0.3s ease;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+        }
+
+        .nav-link:hover::after {
+            width: 100%;
+            /* Underline animation */
+        }
+    </style>
 </head>
+
 
 <body>
     <header>
@@ -14,7 +44,7 @@
                 <!-- Logo -->
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img src="{{ asset('images/logo/favicon.ico') }}" alt="Logo" width="30" height="30"
-                        class="d-inline-block align-text-top">
+                        class="d-inline-block align-text-top rounded-circle">
                 </a>
                 <!-- Toggle Button for Small Screens -->
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
